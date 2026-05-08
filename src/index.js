fd22f0215ffed3a4657e2d6126e7eaef98e04592
Yogesh Joshi <work.yogeshjoshi@gmail.com>
2026-05-08 09:24:22 +0530

    update: update cache worker and index file to run background worker on render

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import { app } from './app.js';
 import connectDB from './config/db.js';
 import http from "node:http"
 import { initSocket } from './config/socket.js';
+import "./workers/reminder.worker.js"
+import { initCacheWorker } from './workers/cache.worker.js';
 
 try {
   await connectDB();
+
+  initCacheWorker().catch(err=>console.log("Cache worker fail ", err))
   const PORT = process.env.PORT;
   const server = http.createServer(app);
 
--- src/workers/cache.worker.js
+++ src/workers/cache.worker.js
@@ -9,32 +9,33 @@ const cacheSub = new Redis({
   password: process.env.REDIS_PASSWORD,
 });
 
-await cacheSub.subscribe('cache-events');
+export const initCacheWorker = async () => {
+  await cacheSub.subscribe('cache-events');
 
-cacheSub.on('message', async (channel, message) => {
-  try {
-    if (channel !== 'cache-events') return;
+  cacheSub.on('message', async (channel, message) => {
+    try {
+      if (channel !== 'cache-events') return;
 
-    const event = JSON.parse(message);
-    if (event.type !== 'INVALIDATE_TASKS') return;
+      const event = JSON.parse(message);
+      if (event.type !== 'INVALIDATE_TASKS') return;
 
-    // PERSONAL
-    await invalidateCache('tasks', event.userId);
+      // PERSONAL
+      await invalidateCache('tasks', event.userId);
 
-    if (event.assignedTo && event.assignedTo !== event.userId) {
-      await invalidateCache('tasks', event.assignedTo);
+      if (event.assignedTo && event.assignedTo !== event.userId) {
+        await invalidateCache('tasks', event.assignedTo);
+      }
+
+      if (event.oldAssignedTo && event.oldAssignedTo !== event.assignedTo) {
+        await invalidateCache('tasks', event.oldAssignedTo);
+      }
+
+      // BOARD
+      if (event.boardId) {
+        await invalidateCache('boardTasks', event.boardId);
+      }
+    } catch (error) {
+      console.log('Cache worker error: ', error.message);
     }
-
-    if (event.oldAssignedTo && event.oldAssignedTo !== event.assignedTo) {
-      await invalidateCache('tasks', event.oldAssignedTo);
-    }
-
-    // BOARD
-    if (event.boardId) {
-      await invalidateCache('boardTasks', event.boardId);
-    }
-
-  } catch (error) {
-    console.log('Cache worker error: ', error.message);
-  }
-});
+  });
+};
